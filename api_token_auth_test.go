431806b74d45f1da2e1f8ef0799ad7a29285e0e2
megadotnet <wintersunair@gmail.com>
2022-03-09 17:33:18 +0800

    added register method

--- api_token_auth_test.go
+++ api_token_auth_test.go
@@ -3,12 +3,18 @@ package main
 import (
 	sw "app/swagger"
 	"encoding/base64"
+	"math/rand"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/antihax/optional"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
+
+const SMS_CODE string = "898017"
+const ACCOUNT_PREFIX = "+218 91"
 
 func TestLogin(t *testing.T) {
 
@@ -16,7 +22,7 @@ func TestLogin(t *testing.T) {
 		LoginType: 0,
 		UserName:  base64.StdEncoding.EncodeToString([]byte("kmdxdk1@hotmail.com")),
 		Password:  base64.StdEncoding.EncodeToString([]byte("1234qwer")),
-		SmsCode:   "898017",
+		SmsCode:   SMS_CODE,
 		ImageKey:  "2323",
 		ImageCode: "8980"}
 
@@ -37,4 +43,35 @@ func TestLogin(t *testing.T) {
 		t.Log(err)
 	}
 
+}
+
+func TestRegister(t *testing.T) {
+
+	registerModel := sw.WctApiApplicationAuthorizationDtoRegisterModel{
+		BindAccount: ACCOUNT_PREFIX + strconv.Itoa(GenerateRandInt(1, 9999999)),
+		SmsCode:     SMS_CODE,
+		Password:    "@234qwer"}
+
+	registerModelpost := sw.TokenAuthApiApiTokenAuthRegisterPostOpts{
+		Body: optional.NewInterface(registerModel),
+	}
+
+	resp, r, err := client.TokenAuthApi.ApiTokenAuthRegisterPost(context.Background(), &registerModelpost)
+	assert := assert.New(t)
+	if err != nil {
+		t.Errorf("Error while get TestRegister")
+		t.Log(err)
+	} else {
+		t.Log(registerModel.BindAccount)
+		assert.NotNil(resp, "Result should  not be null")
+	}
+	if r.StatusCode != 200 {
+		t.Log(err)
+	}
+
+}
+
+func GenerateRandInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano()) //随机种子
+	return rand.Intn(max-min) + min
 }
