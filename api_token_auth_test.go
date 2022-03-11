063d72ea2267e82093014a4275d95baee3944525
megadotnet <wintersunair@gmail.com>
2022-03-11 22:19:27 +0800

    update concurreny

--- api_token_auth_test.go
+++ api_token_auth_test.go
@@ -65,7 +65,7 @@ func TestRegister(t *testing.T) {
 func TestRegisterConcurrency(t *testing.T) {
 	errc := make(chan error)
 	sum := 0
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 100; i++ {
 		go func() {
 			resp, r, err := CreateRegisterProc(t)
 			if err != nil {
